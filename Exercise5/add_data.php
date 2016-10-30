86d765243945456fb6ebdf57681683b87c2df0f4
Gabriel Angelo A. Ngceen <gngceen@gmail.com>
2016-10-31 03:57:45 +0800

    Edited add_data.php

--- Exercise5/add_data.php
+++ Exercise5/add_data.php
@@ -3,14 +3,15 @@ include_once 'dbconfig.php';
 if(isset($_POST['btn-save']))
 {
  // variables for input data
- $first_name = $_POST['first_name'];
- $last_name = $_POST['last_name'];
- $city_name = $_POST['city_name'];
+ $full_name = $_POST['full_name'];
+ $nickname = $_POST['nickname'];
+ $email = $_POST['email'];
  // variables for input data
  
  // sql query for inserting data into database
  
-        $sql_query = "INSERT INTO userinfo(first_name,last_name,user_city) VALUES('$first_name','$last_name','$city_name')";
+        $sql_query = "INSERT INTO userinfo(full_name,nickname,email) 
+		VALUES('$full_name','$nickname','$email')";
  mysql_query($sql_query);
         
         // sql query for inserting data into database
@@ -36,13 +37,25 @@ if(isset($_POST['btn-save']))
     <td align="center"><a href="index.php">back to main page</a></td>
     </tr>
     <tr>
-    <td><input type="text" name="first_name" placeholder="First Name" required /></td>
+    <td><input type="text" name="full_name" placeholder="Full Name" required /></td>
     </tr>
     <tr>
-    <td><input type="text" name="last_name" placeholder="Last Name" required /></td>
+    <td><input type="text" name="nickname" placeholder="Nickname" required /></td>
     </tr>
     <tr>
-    <td><input type="text" name="city_name" placeholder="City" required /></td>
+    <td><input type="text" name="email" placeholder="Email" required /></td>
+    </tr>
+	<tr>
+    <td><input type="text" name="address" placeholder="Address" /></td>
+    </tr>
+	<tr>
+    <td><input type="text" name="gender" placeholder="Gender" required /></td>
+    </tr>
+	<tr>
+    <td><input type="text" name="cp_num" placeholder="Cellphone number" required /></td>
+    </tr>
+	<tr>
+    <td><input type="text" name="comment" placeholder="Comment" required /></td>
     </tr>
     <tr>
     <td><button type="submit" name="btn-save"><strong>SAVE</strong></button></td>
