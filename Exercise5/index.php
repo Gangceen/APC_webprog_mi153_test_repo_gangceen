abc6b23d5aa8025f8f4daad1d7667ff1f9382825
Gabriel Angelo A. Ngceen <gngceen@gmail.com>
2016-10-31 04:19:14 +0800

    Edited index.php

--- Exercise5/index.php
+++ Exercise5/index.php
@@ -4,19 +4,15 @@ include_once 'dbconfig.php';
 // delete condition
 if(isset($_GET['delete_id']))
 {
- $sql_query="DELETE FROM users WHERE user_id=".$_GET['delete_id'];
+ $sql_query="DELETE FROM userinfo WHERE user_id=".$_GET['delete_id'];
  mysql_query($sql_query);
  header("Location: $_SERVER[PHP_SELF]");
 }
 // delete condition
 ?>
 
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
+<html>
 <head>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title>CRUD Operations With PHP and MySql - By Cleartuts</title>
-<link rel="stylesheet" href="style.css" type="text/css" />
 <script type="text/javascript">
 function edt_id(id)
 {
@@ -49,13 +45,17 @@ function delete_id(id)
     <tr>
     <th colspan="5"><a href="add_data.php">add data here.</a></th>
     </tr>
-    <th>First Name</th>
-    <th>Last Name</th>
-    <th>City Name</th>
+    <th>Full Name</th>
+    <th>Nickname</th>
+    <th>Email</th>
+	<th>Address</th>
+	<th>Gender</th>
+	<th>Cellphone number</th>
+	<th>Comments</th>
     <th colspan="2">Operations</th>
     </tr>
     <?php
- $sql_query="SELECT * FROM users";
+ $sql_query="SELECT * FROM userinfo";
  $result_set=mysql_query($sql_query);
  while($row=mysql_fetch_row($result_set))
  {
@@ -64,6 +64,10 @@ function delete_id(id)
         <td><?php echo $row[1]; ?></td>
         <td><?php echo $row[2]; ?></td>
         <td><?php echo $row[3]; ?></td>
+		<td><?php echo $row[4]; ?></td>
+        <td><?php echo $row[5]; ?></td>
+        <td><?php echo $row[6]; ?></td>
+		<td><?php echo $row[7]; ?></td>
   <td align="center"><a href="javascript:edt_id('<?php echo $row[0]; ?>')"><img src="b_edit.png" align="EDIT" /></a></td>
         <td align="center"><a href="javascript:delete_id('<?php echo $row[0]; ?>')"><img src="b_drop.png" align="DELETE" /></a></td>
         </tr>
