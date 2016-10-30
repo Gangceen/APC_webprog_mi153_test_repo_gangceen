10c09d1cc1adc41ac30bea70b5052569902b6c05
Gabriel Angelo A. Ngceen <gngceen@gmail.com>
2016-10-31 04:11:01 +0800

    Fixed add_data.php Edited edit_data.php

--- Exercise5/add_data.php
+++ Exercise5/add_data.php
@@ -22,7 +22,7 @@ if(isset($_POST['btn-save']))
  
 }
 ?>
-<html
+<html>
 <head>
 </head>
 <body>
--- Exercise5/edit_data.php
+++ Exercise5/edit_data.php
@@ -47,12 +47,8 @@ if(isset($_POST['btn-cancel']))
  header("Location: index.php");
 }
 ?>
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
+<html>
 <head>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title>CRUD Operations With PHP and MySql - By Cleartuts</title>
-<link rel="stylesheet" href="style.css" type="text/css" />
 </head>
 <body>
 <center>
@@ -68,13 +64,28 @@ if(isset($_POST['btn-cancel']))
     <form method="post">
     <table align="center">
     <tr>
-    <td><input type="text" name="first_name" placeholder="First Name" value="<?php echo $fetched_row['first_name']; ?>" required /></td>
+    <td align="center"><a href="index.php">back to main page</a></td>
     </tr>
     <tr>
-    <td><input type="text" name="last_name" placeholder="Last Name" value="<?php echo $fetched_row['last_name']; ?>" required /></td>
+    <td><input type="text" name="full_name" placeholder="Full Name" required /></td>
     </tr>
     <tr>
-    <td><input type="text" name="city_name" placeholder="City" value="<?php echo $fetched_row['user_city']; ?>" required /></td>
+    <td><input type="text" name="nickname" placeholder="Nickname" required /></td>
+    </tr>
+    <tr>
+    <td><input type="text" name="email" placeholder="Email" required /></td>
+    </tr>
+	<tr>
+    <td><input type="text" name="address" placeholder="Address" /></td>
+    </tr>
+	<tr>
+    <td><input type="text" name="gender" placeholder="Gender" required /></td>
+    </tr>
+	<tr>
+    <td><input type="text" name="cp_num" placeholder="Cellphone number" required /></td>
+    </tr>
+	<tr>
+    <td><input type="text" name="comment" placeholder="Comment" required /></td>
     </tr>
     <tr>
     <td>
