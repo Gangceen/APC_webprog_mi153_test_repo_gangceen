4b998fc1049be2d8065ca04ae06084b33ec956de
Gabriel Angelo A. Ngceen <gngceen@gmail.com>
2016-11-14 20:13:20 +0800

    edited config.php

--- Exercise6/CodeIgniter/application/config/config.php
+++ Exercise6/CodeIgniter/application/config/config.php
@@ -35,7 +35,7 @@ $config['base_url'] = 'http://localhost/APC_webprog_mi153_test_repo_gangceen/Exe
 | variable so that it is blank.
 |
 */
-$config['index_page'] = 'index.php';
+$config['index_page'] = 'website.php';
 
 /*
 |--------------------------------------------------------------------------
