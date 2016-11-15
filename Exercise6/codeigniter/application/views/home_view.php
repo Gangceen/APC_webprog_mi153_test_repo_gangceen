06a339e97baa788e20d4e59dddd1b3d4cf756d67
Gabriel Angelo A. Ngceen <gngceen@gmail.com>
2016-11-15 12:10:03 +0800

    Created new exercise6

---
+++ Exercise6/codeigniter/application/views/home_view.php
@@ -0,0 +1,57 @@
+<html>
+	<head>
+		<div class="header">
+		<h1><i><center>
+		Le Personal Webpage
+		</center></i></h1>
+		</div>
+		<link href="webpagestyle.css" type="text/css" rel="stylesheet" />
+		
+		<style>
+			table, th, td {
+				border: 1px solid blue;
+				color: white;
+				width: 75%;
+			}
+
+			body{
+				background-image: url("background.jpg");
+				color: #66a3ff;
+			}
+
+			.header, .footer{
+				background-color: #1a75ff;
+				color: #f9f9f9;
+				padding: 10px;
+			}
+		</style>
+			</head>
+	
+	
+		<body><center>
+			<table>
+			<tr>
+				<th>Field</th>
+				<th>Information</th>
+			</tr>
+			<tr>
+				<td>Name</td>
+				<td>Gabriel Angelo Agito Ngceen</td>
+			</tr>
+			<tr>
+				<td>Nickname:</td>
+				<td>Gabby/Gab</td>
+			</tr>
+			<tr>
+				<td>Birthdate:</td>
+				<td>January 1, 2000</td>
+			</tr>
+			<tr>
+				<td>Age:</td>
+				<td>16</td>
+			</tr>
+			<tr>
+				<td>Course:</td>
+				<td>BS Information Technology, Specializing in mobile and internet technology</td>
+			</tr>
+			</table>
