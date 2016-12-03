6389e0fc90df3a0174dfa5667305dc11717b8bbb
Gabriel Angelo A. Ngceen <gngceen@gmail.com>
2016-12-03 18:40:20 +0800

    Edited index.php

--- Exercise6/codeigniter/application/views/news/index.php
+++ Exercise6/codeigniter/application/views/news/index.php
@@ -10,7 +10,7 @@
 		}
 
 		body{
-			background-image: url("background.jpg");
+			background-image: url(<?=base_url()?>Images/background.jpg);
 			color: #66a3ff;
 		}
 
@@ -61,18 +61,18 @@
 
 			<p>
 			<br>Favorite websites as pastime:
-			<br><a href= "https://www.youtube.com"><img border="0" alt="Youtube" src="youtube.jpg" width=100 height=100></a>
-			<a href= "https://www.facebook.com"><img border="0" alt="Facebook" src="facebook.jpg" width=100 height=100></a>
-			<a href= "https://www.twitter.com"><img border="0" alt="Twitter" src="twitter.jpg" width=100 height=100></a>
+			<br><a href= "https://www.youtube.com"><img border="0" alt="Youtube" src="<?php echo base_url('Images/youtube.jpg');?>" width=100 height=100></a>
+			<a href= "https://www.facebook.com"><img border="0" alt="Facebook" src="<?php echo base_url('Images/facebook.jpg');?>" width=100 height=100></a>
+			<a href= "https://www.twitter.com"><img border="0" alt="Twitter" src="<?php echo base_url('Images/twitter.jpg');?>" width=100 height=100></a>
 			</p>
 
 			<p>
 			<br>Favorite games:
-			<br><a href= "https://lol.garena.ph/"><img border="0" alt="League of Legends" src="lol.jpg" width=100 height=100></a>
-			<a href= "http://store.steampowered.com/app/316010/"><img border="0" alt="Magic:The Gathering" src="MTG.jpg" 
+			<br><a href= "https://lol.garena.ph/"><img border="0" alt="League of Legends" src="<?php echo base_url('Images/lol.jpg');?>" width=100 height=100></a>
+			<a href= "http://store.steampowered.com/app/316010/"><img border="0" alt="Magic:The Gathering" src="<?php echo base_url('Images/MTG.jpg');?>" 
 
 width=100 height=100></a>
-			<a href= "https://minecraft.net/en/"><img border="0" alt="Minecraft" src="minecraft.jpg" width=100 height=100></a>
+			<a href= "https://minecraft.net/en/"><img border="0" alt="Minecraft" src="<?php echo base_url('Images/minecraft.jpg');?>" width=100 height=100></a>
 			</p>
 
 			<h2> Trivia! </h2>
