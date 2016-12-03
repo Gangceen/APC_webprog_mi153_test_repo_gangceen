225ace4103f1302d7bd04130c7d84d02c54a9e22
Gabriel Angelo A. Ngceen <gngceen@gmail.com>
2016-12-03 18:02:30 +0800

    Edited index.php

--- Exercise6/codeigniter/application/views/news/index.php
+++ Exercise6/codeigniter/application/views/news/index.php
@@ -1,5 +1,130 @@
-<h2><?php echo $title; ?></h2>
- 
+<html>
+
+	<head>
+		<h1><i><center><font color = #1a75ff>Le Personal Webpage</font></center></i></h1>
+	<style>
+		table, th, td {
+			border: 1px solid blue;
+			color: #66a3ff;
+			width: 50%;
+		}
+
+		body{
+			background-image: url("background.jpg");
+			color: #66a3ff;
+		}
+
+		#q1, #q2, #q3, #q4, #q5{
+			color: #ff0000;
+		}
+
+		.error {color: #FF0000;}
+	</style>
+	</head>
+
+		<body>
+		<center>
+			<table id="table">
+			<tr>
+				<th>Field</th>
+				<th>Information</th>
+			</tr>
+			<tr>
+				<td>Name</td>
+				<td>Gabriel Angelo Agito Ngceen</td>
+			</tr>
+			<tr>
+				<td>Nickname:</td>
+				<td>Gabby/Gab</td>
+			</tr>
+			<tr>
+				<td>Birthdate:</td>
+				<td>January 1, 2000</td>
+			</tr>
+			<tr>
+				<td>Age:</td>
+				<td>16</td>
+			</tr>
+			<tr>
+				<td>Course:</td>
+				<td>BS Information Technology, Specializing in mobile and internet technology</td>
+			</tr>
+			</table>
+
+			<button type="button" onclick="document.getElementById('table').style.fontSize='35px'">
+			Enlarge
+			</button>
+
+			<p>
+			<br>Favorite websites as pastime:
+			<br><a href= "https://www.youtube.com"><img border="0" alt="Youtube" src="youtube.jpg" width=100 height=100></a>
+			<a href= "https://www.facebook.com"><img border="0" alt="Facebook" src="facebook.jpg" width=100 height=100></a>
+			<a href= "https://www.twitter.com"><img border="0" alt="Twitter" src="twitter.jpg" width=100 height=100></a>
+			</p>
+
+			<p>
+			<br>Favorite games:
+			<br><a href= "https://lol.garena.ph/"><img border="0" alt="League of Legends" src="lol.jpg" width=100 height=100></a>
+			<a href= "http://store.steampowered.com/app/316010/"><img border="0" alt="Magic:The Gathering" src="MTG.jpg" 
+
+width=100 height=100></a>
+			<a href= "https://minecraft.net/en/"><img border="0" alt="Minecraft" src="minecraft.jpg" width=100 height=100></a>
+			</p>
+
+			<h2> Trivia! </h2>
+
+			<p>
+				1. My most common gaming username:
+			</p>
+
+			<p id="q1">
+				ANSWER
+			</p>
+
+			<button type="button" onclick="document.getElementById('q1').innerHTML = 'NZAAXD or VoidDoubleA'">Click to see answer</button>
+
+			<p>
+				2. My favorite subject:
+			</p>
+
+			<p id="q2">
+				ANSWER
+			</p>
+
+			<button type="button" onclick="document.getElementById('q2').innerHTML = 'Anything that has Math'">Click to see answer</button>
+
+			<p>
+				3. How long have I been playing Magic?
+			</p>
+
+			<p id="q3">
+				ANSWER
+			</p>
+
+			<button type="button" onclick="document.getElementById('q3').innerHTML = 'Started on September 2016'">Click to see answer</button>
+
+			<p>
+				4. What else do you do in your free time?
+			</p>
+
+			<p id="q4">
+				ANSWER
+			</p>
+
+			<button type="button" onclick="document.getElementById('q4').innerHTML = 'Play on my PS3, or play basketball with friends.'">Click to see answer</button>
+
+			<p>
+				5. Where do you go to buy magic cards?
+			</p>
+
+			<p id="q5">
+				ANSWER
+			</p>
+
+			<button type="button" onclick="document.getElementById('q5').innerHTML = 'Hobby shops in taft or SM Megamall, or in Neutral grounds.'">Click to see answer</button>
+
+			</center>
+
 <table border='1' cellpadding='4'>
     <tr>
         <td><strong>Name</strong></td>
