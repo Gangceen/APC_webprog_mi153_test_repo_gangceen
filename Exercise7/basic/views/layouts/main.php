2ebd4b451ac660f1e75f622b0a9fdffbba40ad5c
Gabriel Angelo A. Ngceen <gngceen@gmail.com>
2016-12-10 20:34:35 +0800

    edited main.php

--- Exercise7/basic/views/layouts/main.php
+++ Exercise7/basic/views/layouts/main.php
@@ -27,7 +27,7 @@ AppAsset::register($this);
 <div class="wrap">
     <?php
     NavBar::begin([
-        'brandLabel' => 'My Company',
+        'brandLabel' => 'Exercise 7 Website',
         'brandUrl' => Yii::$app->homeUrl,
         'options' => [
             'class' => 'navbar-inverse navbar-fixed-top',
@@ -38,7 +38,8 @@ AppAsset::register($this);
         'items' => [
             ['label' => 'Home', 'url' => ['/site/index']],
             ['label' => 'About', 'url' => ['/site/about']],
-            ['label' => 'Contact', 'url' => ['/site/contact']],
+            ['label' => 'Trivia', 'url' => ['/site/trivia']],
+			['label' => 'Comment', 'url' => ['/site/comment']]
             Yii::$app->user->isGuest ? (
                 ['label' => 'Login', 'url' => ['/site/login']]
             ) : (
