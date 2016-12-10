4419fa892f5119b5d108b1652fb03c55f7a66999
Gabriel Angelo A. Ngceen <gngceen@gmail.com>
2016-12-10 20:38:17 +0800

    edited main.php

--- Exercise7/basic/views/layouts/main.php
+++ Exercise7/basic/views/layouts/main.php
@@ -40,18 +40,6 @@ AppAsset::register($this);
             ['label' => 'About', 'url' => ['/site/about']],
             ['label' => 'Trivia', 'url' => ['/site/trivia']],
 			['label' => 'Comment', 'url' => ['/site/comment']]
-            Yii::$app->user->isGuest ? (
-                ['label' => 'Login', 'url' => ['/site/login']]
-            ) : (
-                '<li>'
-                . Html::beginForm(['/site/logout'], 'post')
-                . Html::submitButton(
-                    'Logout (' . Yii::$app->user->identity->username . ')',
-                    ['class' => 'btn btn-link logout']
-                )
-                . Html::endForm()
-                . '</li>'
-            )
         ],
     ]);
     NavBar::end();
