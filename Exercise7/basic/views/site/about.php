244068716ca7f2112df471e9ce364e0636cff9d3
Gabriel Angelo A. Ngceen <gngceen@gmail.com>
2016-12-10 21:02:24 +0800

    edited about.php

--- Exercise7/basic/views/site/about.php
+++ Exercise7/basic/views/site/about.php
@@ -4,15 +4,42 @@
 
 use yii\helpers\Html;
 
-$this->title = 'About';
+$this->title = 'About the user';
 $this->params['breadcrumbs'][] = $this->title;
 ?>
+	<style>
+		table, th, td {
+			border: 4px solid black;
+			color: #0039e6;
+			height: 10%
+			width: 50%;
+		}
+	</style>
 <div class="site-about">
-    <h1><?= Html::encode($this->title) ?></h1>
-
-    <p>
-        This is the About page. You may modify the following file to customize its content:
-    </p>
-
-    <code><?= __FILE__ ?></code>
+			<table id="table" align="center">
+			<tr>
+				<th>Field</th>
+				<th>Information</th>
+			</tr>
+			<tr>
+				<td>Name</td>
+				<td>Gabriel Angelo Agito Ngceen</td>
+			</tr>
+			<tr>
+				<td>Nickname:</td>
+				<td>Gabby/Gab</td>
+			</tr>
+			<tr>
+				<td>Birthdate:</td>
+				<td>January 1, 2000</td>
+			</tr>
+			<tr>
+				<td>Age:</td>
+				<td>16</td>
+			</tr>
+			<tr>
+				<td>Course:</td>
+				<td>BS Information Technology, Specializing in mobile and internet technology</td>
+			</tr>
+			</table>
 </div>
