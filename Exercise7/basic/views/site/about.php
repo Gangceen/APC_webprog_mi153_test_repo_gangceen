72a08ba433c06ca613e1066c818b9b00e78a08fd
Gabriel Angelo A. Ngceen <gngceen@gmail.com>
2016-12-11 14:43:29 +0800

    edited files

--- Exercise7/basic/views/site/about.php
+++ Exercise7/basic/views/site/about.php
@@ -4,15 +4,58 @@
 
 use yii\helpers\Html;
 
-$this->title = 'About';
+$this->title = 'About the user';
 $this->params['breadcrumbs'][] = $this->title;
 ?>
+	<style>
+		table, th, td {
+			border: 4px solid black;
+			color: #0039e6;
+			height: 10%
+			width: 50%;
+		}
+	</style>
 <div class="site-about">
-    <h1><?= Html::encode($this->title) ?></h1>
+			<table id="table" align="center">
+			<tr>
+				<th>Field</th>
+				<th>Information</th>
+			</tr>
+			<tr>
+				<td>Name</td>
+				<td>Gabriel Angelo Agito Ngceen</td>
+			</tr>
+			<tr>
+				<td>Nickname:</td>
+				<td>Gabby/Gab</td>
+			</tr>
+			<tr>
+				<td>Birthdate:</td>
+				<td>January 1, 2000</td>
+			</tr>
+			<tr>
+				<td>Age:</td>
+				<td>16</td>
+			</tr>
+			<tr>
+				<td>Course:</td>
+				<td>BS Information Technology, Specializing in mobile and internet technology</td>
+			</tr>
+			</table>
+			
+			<p><center>
+			<br>Favorite websites as pastime:
+			<br><a href= "https://www.youtube.com"><img border="0" alt="Youtube" src="youtube.jpg" width=100 height=100></a>
+			<a href= "https://www.facebook.com"><img border="0" alt="Facebook" src="facebook.jpg" width=100 height=100></a>
+			<a href= "https://www.twitter.com"><img border="0" alt="Twitter" src="twitter.jpg" width=100 height=100></a>
+			</p>
 
-    <p>
-        This is the About page. You may modify the following file to customize its content:
-    </p>
+			<p>
+			<br>Favorite games:
+			<br><a href= "https://lol.garena.ph/"><img border="0" alt="League of Legends" src="lol.jpg" width=100 height=100></a>
+			<a href= "http://store.steampowered.com/app/316010/"><img border="0" alt="Magic:The Gathering" src="MTG.jpg" 
 
-    <code><?= __FILE__ ?></code>
+width=100 height=100></a>
+			<a href= "https://minecraft.net/en/"><img border="0" alt="Minecraft" src="minecraft.jpg" width=100 height=100></a>
+			</p>
 </div>
--- Exercise7/basic/views/site/index.php
+++ Exercise7/basic/views/site/index.php
@@ -2,50 +2,43 @@
 
 /* @var $this yii\web\View */
 
-$this->title = 'My Yii Application';
+$this->title = 'Exercise 7 Application';
 ?>
 <div class="site-index">
 
     <div class="jumbotron">
-        <h1>Congratulations!</h1>
+        <h1>Welcome!</h1>
 
-        <p class="lead">You have successfully created your Yii-powered application.</p>
+        <p class="lead">This is my Exercise 7 Application for WEBPROG</p>
+		
+		<p>Want to learn how to use Yii? Click the button below!</p>
 
         <p><a class="btn btn-lg btn-success" href="http://www.yiiframework.com">Get started with Yii</a></p>
-    </div>
+		
+		<p>Press the tabs above to navigate throughout the website!</p>
+		
+	</div>
 
     <div class="body-content">
-
+				
         <div class="row">
             <div class="col-lg-4">
-                <h2>Heading</h2>
+                <h2>About</h2>
 
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
+                <p>See basic details about the creator of this website!</p>
 
-                <p><a class="btn btn-default" href="http://www.yiiframework.com/doc/">Yii Documentation &raquo;</a></p>
             </div>
             <div class="col-lg-4">
-                <h2>Heading</h2>
+                <h2>Trivia</h2>
 
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
+                <p>Learn fun facts about the creator!</p>
 
-                <p><a class="btn btn-default" href="http://www.yiiframework.com/forum/">Yii Forum &raquo;</a></p>
             </div>
             <div class="col-lg-4">
-                <h2>Heading</h2>
+                <h2>Comment</h2>
 
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
+                <p>Comment on the website's details, leaving your personal details...</p>
 
-                <p><a class="btn btn-default" href="http://www.yiiframework.com/extensions/">Yii Extensions &raquo;</a></p>
             </div>
         </div>
 
