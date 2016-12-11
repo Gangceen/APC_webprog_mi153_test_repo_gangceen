b0d90b8b4b02249dbc1005d62c4158afd46b141d
Gabriel Angelo A. Ngceen <gngceen@gmail.com>
2016-12-11 13:22:37 +0800

    edited trivia.php

--- Exercise7/basic/views/site/trivia.php
+++ Exercise7/basic/views/site/trivia.php
@@ -1,68 +1,51 @@
-<?php
+<h2> Trivia! </h2>
 
-/* @var $this yii\web\View */
-/* @var $form yii\bootstrap\ActiveForm */
-/* @var $model app\models\ContactForm */
+			<p>
+				1. My most common gaming username:
+			</p>
 
-use yii\helpers\Html;
-use yii\bootstrap\ActiveForm;
-use yii\captcha\Captcha;
+			<p id="q1">
+				ANSWER
+			</p>
 
-$this->title = 'Contact';
-$this->params['breadcrumbs'][] = $this->title;
-?>
-<div class="site-contact">
-    <h1><?= Html::encode($this->title) ?></h1>
+			<button type="button" onclick="document.getElementById('q1').innerHTML = 'NZAAXD or VoidDoubleA'">Click to see answer</button>
 
-    <?php if (Yii::$app->session->hasFlash('contactFormSubmitted')): ?>
+			<p>
+				2. My favorite subject:
+			</p>
 
-        <div class="alert alert-success">
-            Thank you for contacting us. We will respond to you as soon as possible.
-        </div>
+			<p id="q2">
+				ANSWER
+			</p>
 
-        <p>
-            Note that if you turn on the Yii debugger, you should be able
-            to view the mail message on the mail panel of the debugger.
-            <?php if (Yii::$app->mailer->useFileTransport): ?>
-                Because the application is in development mode, the email is not sent but saved as
-                a file under <code><?= Yii::getAlias(Yii::$app->mailer->fileTransportPath) ?></code>.
-                Please configure the <code>useFileTransport</code> property of the <code>mail</code>
-                application component to be false to enable email sending.
-            <?php endif; ?>
-        </p>
+			<button type="button" onclick="document.getElementById('q2').innerHTML = 'Anything that has Math'">Click to see answer</button>
 
-    <?php else: ?>
+			<p>
+				3. How long have I been playing Magic?
+			</p>
 
-        <p>
-            If you have business inquiries or other questions, please fill out the following form to contact us.
-            Thank you.
-        </p>
+			<p id="q3">
+				ANSWER
+			</p>
 
-        <div class="row">
-            <div class="col-lg-5">
+			<button type="button" onclick="document.getElementById('q3').innerHTML = 'Started on September 2016'">Click to see answer</button>
 
-                <?php $form = ActiveForm::begin(['id' => 'contact-form']); ?>
+			<p>
+				4. What else do you do in your free time?
+			</p>
 
-                    <?= $form->field($model, 'name')->textInput(['autofocus' => true]) ?>
+			<p id="q4">
+				ANSWER
+			</p>
 
-                    <?= $form->field($model, 'email') ?>
+			<button type="button" onclick="document.getElementById('q4').innerHTML = 'Play on my PS3, or play basketball with friends.'">Click to see answer</button>
 
-                    <?= $form->field($model, 'subject') ?>
+			<p>
+				5. Where do you go to buy magic cards?
+			</p>
 
-                    <?= $form->field($model, 'body')->textarea(['rows' => 6]) ?>
+			<p id="q5">
+				ANSWER
+			</p>
 
-                    <?= $form->field($model, 'verifyCode')->widget(Captcha::className(), [
-                        'template' => '<div class="row"><div class="col-lg-3">{image}</div><div class="col-lg-6">{input}</div></div>',
-                    ]) ?>
-
-                    <div class="form-group">
-                        <?= Html::submitButton('Submit', ['class' => 'btn btn-primary', 'name' => 'contact-button']) ?>
-                    </div>
-
-                <?php ActiveForm::end(); ?>
-
-            </div>
-        </div>
-
-    <?php endif; ?>
-</div>
+			<button type="button" onclick="document.getElementById('q5').innerHTML = 'Hobby shops in taft or SM Megamall, or in Neutral grounds.'">Click to see answer</button>
