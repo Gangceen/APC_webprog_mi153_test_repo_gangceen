2085ed0f8c0afee226010b1b9fd39d9a6ca6c419
Gabriel Angelo A. Ngceen <gngceen@gmail.com>
2016-12-13 12:37:08 +0800

    edited main.php

--- Exercise7/basic/views/layouts/main.php
+++ Exercise7/basic/views/layouts/main.php
@@ -38,7 +38,8 @@ AppAsset::register($this);
         'items' => [
             ['label' => 'Home', 'url' => ['/site/index']],
             ['label' => 'About', 'url' => ['/site/about']],
-            ['label' => 'Trivia', 'url' => ['/site/trivia']]
+            ['label' => 'Trivia', 'url' => ['/site/trivia']],
+			['label' => 'Comments', 'url' => ['/userinfo]]
         ],
     ]);
     NavBar::end();
