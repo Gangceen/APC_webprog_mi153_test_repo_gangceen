56fcb64f92730fbcab800bf68df78f50fa49dd56
Gabriel Angelo A. Ngceen <gngceen@gmail.com>
2016-12-22 04:08:30 +0800

    edited main.php

--- Exercise7/basic/views/layouts/main.php
+++ Exercise7/basic/views/layouts/main.php
@@ -38,7 +38,7 @@ AppAsset::register($this);
         'items' => [
             ['label' => 'Home', 'url' => ['/site/index']],
             ['label' => 'About', 'url' => ['/site/about']],
-            ['label' => 'Trivia', 'url' => ['/site/trivia']],
+            ['label' => 'Trivia', 'url' => ['/trivia/index']],
 			['label' => 'Comments', 'url' => ['/userinfo/index']]
         ],
     ]);
