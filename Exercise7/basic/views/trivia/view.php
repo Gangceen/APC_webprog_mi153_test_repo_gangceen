e7a5c8d606e79da87408f1ec964e9d488259d377
Gabriel Angelo A. Ngceen <gngceen@gmail.com>
2016-12-22 04:33:26 +0800

    edited view.php

--- Exercise7/basic/views/trivia/view.php
+++ Exercise7/basic/views/trivia/view.php
@@ -13,18 +13,7 @@ $this->params['breadcrumbs'][] = $this->title;
 <div class="trivia-view">
 
     <h1><?= Html::encode($this->title) ?></h1>
-
-    <p>
-        <?= Html::a('Update', ['update', 'id' => $model->User_ID], ['class' => 'btn btn-primary']) ?>
-        <?= Html::a('Delete', ['delete', 'id' => $model->User_ID], [
-            'class' => 'btn btn-danger',
-            'data' => [
-                'confirm' => 'Are you sure you want to delete this item?',
-                'method' => 'post',
-            ],
-        ]) ?>
-    </p>
-
+	
     <?= DetailView::widget([
         'model' => $model,
         'attributes' => [
